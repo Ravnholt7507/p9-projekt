1560d4da39357c069b6fcd8c639f914215edca44
Ravnholt7507 <56935446+Ravnholt7507@users.noreply.github.com>
2024-11-28 13:26:35 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -27,15 +27,3 @@ int main(){
 
     return 0;
 }
-
-
-    // AggregatedFlexOffer obj(0, flexOffers);
-    // obj.pretty_print();
-    // ScheduledFlexOffer SFO(obj);
-    // SFO.print_schedule();
-    // SFO.n_to_1_disaggregation(obj.individual_offers, obj);
-
-
-    return 0;
-}
-
