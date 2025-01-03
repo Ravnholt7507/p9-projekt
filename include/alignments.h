0289cb91f8d0bd1b218a9a49a12c0b3a8a769ec8
Mikkel <miksmedt@gmail.com>
2025-01-03 11:22:05 +0100

    added price alignment for tec

--- include/alignments.h
+++ include/alignments.h
@@ -9,4 +9,5 @@ void start_alignment(time_t &aggregated_earliest, time_t &aggregated_latest, tim
 void balance_alignment(time_t &aggregated_earliest, time_t &aggregated_latest, time_t &aggregated_end_time, vector<TimeSlice> &aggregated_profile, int &duration, vector<Flexoffer> offers);
 void balance_alignment(time_t &aggregated_earliest, time_t &aggregated_latest, time_t &aggregated_end_time, vector<TimeSlice> &aggregated_profile, int &duration, double &overall_min, double &overall_max, vector<Tec_flexoffer> offers);
 void priceAwareAlignment(time_t &aggregated_earliest,time_t &aggregated_latest,time_t &aggregated_end_time,std::vector<TimeSlice> &aggregated_profile,int &duration,std::vector<Flexoffer> offers,const std::vector<double> &spotPrices);
+void priceAwareAlignment(time_t &aggregated_earliest,time_t &aggregated_latest,time_t &aggregated_end_time,vector<TimeSlice> &aggregated_profile,int &duration,double &overall_min,double &overall_max,vector<Tec_flexoffer> offers, const vector<double> &spotPrices);
 #endif
